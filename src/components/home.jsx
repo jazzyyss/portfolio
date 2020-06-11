0bc1d2f6bff42a3d81ca76af921864e797ed5861
jaspreetsingh <jaspreetbrar1636@gmail.com>
2020-06-11 19:42:24 +0530

    added content to homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return <div>I am at home</div>;
+    return (
+      <div className="home">
+        <h1>Hi,</h1>
+        <h1>I'm Jaspreet Singh</h1>
+        <h4>MERN stack, React developer.</h4>
+      </div>
+    );
   }
 }
 export default Home;
